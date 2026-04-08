b12d4451b9efa33a639ac9c9cd611320f6faf40d
hrithik87 <140495973+hrithik87@users.noreply.github.com>
2026-04-08 22:36:15 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 
 # --- BEÁLLÍTÁSOK ---
-JELSZO = "kutyajelszo" 
+JELSZO = st.secrets["app_jelszo"] 
 
 st.set_page_config(page_title="FONTOSABB ÁRFOLYAMOK", layout="wide")
 
